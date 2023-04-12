29d47bbdc50b30615a1cf2e0d3267fcf9b54dcfe
daniel-990 <danielarango990@gmail.com>
2023-04-12 16:00:07 -0500

    se cargar los datos nuevos

--- main.js
+++ main.js
@@ -1,43 +1,77 @@
+import datos from './config/config.json' assert {
+    type: 'json'
+}
+
 (function(){
 
     const texto = "dato";
 
+    const id = datos.data.id;
+    const apikey = datos.data.apikey;
+    const serverUrl = datos.data.serverUrl;
+
+    const render = document.getElementById("render");
+
     //parse
-    Parse.initialize("QZC8gDxQTUrzFJAG3CTljdAzeTCjlC9nWvuNtpc2", "vYvHD5qbSbx1tAbv1f29vXnR1UMlEprl5axFmpbn"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-    Parse.serverURL = "https://parseapi.back4app.com";
-    
+    Parse.initialize(id, apikey); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+    Parse.serverURL = serverUrl;
 
     const database = new Parse.Object("danielDb");
-    database.set("titulo", "lorem");
-    database.set("contenido", "lorem");
-    try {
-        let result = database.save();
-        console.log(result);
-    } catch(error) {
-        console.log('Failed to create new object, with error code: ' + error.message);
+
+    //inputs
+    const titulo = document.getElementById("titulo-portafolio");
+    const contenido = document.getElementById("contenido-portafolio");
+    const btnEnviar = document.getElementById("enviar");
+
+    const guardarDatos = () => {
+        if (titulo.value === "" || contenido.value === ""){
+            alert("por favor llene todos los campos");
+        }else{
+            
+            database.set("titulo", titulo.value);
+            database.set("contenido", contenido.value);
+
+            try { 
+                let result = database.save();
+                console.log(result);
+                setTimeout(() => {
+                    titulo.value = "";
+                    contenido.value = "";
+                    location.reload();
+                },1000);
+            } catch(error) {
+                console.log('Failed to create new object, with error code: ' + error.message);
+            }
+        }
     }
 
+    
 
-    // try {
-    //     const autor = carta.get("autor");
-    //     const titulo = carta.get("titulo");
-    //     const contenido = carta.get("contenido");
-    //     console.log(`autor: ${autor}, titulo: ${titulo}, contenido: ${contenido}`);
-    //     setInterval(function(){
-    //         location.reload();
-    //     },2000);
+    const mostarDatos = async () => {
+        const cartas = new Parse.Query("danielDb");
+        try {
+          const resultados = await cartas.find();
+          for (const object of resultados) {
 
-    // } catch (error) {
-    //     console.log(`Failed to retrieve the object, with error code: ${error.message}`);
-    // }
+            const titulo = object.get('titulo');
+            const contenido = object.get('contenido');
 
-    const frases = (frase) => {
-        return `<div class="container">
-            ${frase}
-        </div>`;
+            console.log(`nombre: ${titulo} \nmensaje: ${contenido}`);
+
+            let html = `
+                <i class="bi bi-person-circle"></i> <b style="color: red;">${titulo}</b> dice: <br> <i class="bi bi-chat-dots"></i> ${contenido} <hr>
+            `;
+
+            render.innerHTML += html;
+
+          }
+        } catch (error) {
+          console.error('Error while fetching Project', error);
+        }
     }
 
-    console.log(frases(texto));
-
+    //funciones
+    btnEnviar.addEventListener('click', guardarDatos);
+    mostarDatos();
 
 })();
